880a94335ddabb65c0fde2acd97e3504a9a5e470
Edward Zhang <643431636@qq.com>
2022-08-03 10:23:56 +0800

    Separating editor instance and option in redux

--- src/renderer/slice/CodeEditorSlice.ts
+++ src/renderer/slice/CodeEditorSlice.ts
@@ -4,12 +4,14 @@ import type { RootState } from '../store';
 
 // Define a type for the slice state
 export interface CodeEditorState {
-  editor: editor.IStandaloneCodeEditor | null | undefined;
+  editorInstance: editor.IStandaloneCodeEditor | null | undefined;
+  editorOption: editor.IStandaloneEditorConstructionOptions | undefined;
 }
 
 // Define the initial state using that type
 const initialState: CodeEditorState = {
-  editor: null,
+  editorInstance: null,
+  editorOption: {},
 };
 
 // Create the editor state.
@@ -18,29 +20,30 @@ export const CodeEditorSlice = createSlice({
   initialState,
   reducers: {
     initEditor: (_state: CodeEditorState, action) => {
+      const defaultEditorOption: editor.IStandaloneEditorConstructionOptions = {
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+        theme: 'vs-dark',
+        language: 'typescript',
+        minimap: {
+          enabled: true,
+        },
+        value: '',
+        fontFamily: 'consolas,Microsoft YaHei',
+      };
       return {
-        editor: editor.create(action.payload!, {
-          scrollBeyondLastLine: false,
-          automaticLayout: true,
-          theme: 'vs-dark',
-          language: 'typescript',
-          minimap: {
-            enabled: true,
-          },
-          value: '',
-          fontFamily: 'consolas,Microsoft YaHei',
-        }),
+        editorInstance: editor.create(action.payload!, defaultEditorOption),
+        editorOption: defaultEditorOption,
       };
     },
-    // writeIn: (state: CodeEditorState, action) => {
-    //   state.editor?.getModel()?.setValue(action.payload);
-    // }
   },
 });
 
 export const { initEditor } = CodeEditorSlice.actions;
 
 // Other code such as selectors can use the imported `RootState` type
-export const selectEditor = (state: RootState) => state.codeEditor.editor;
+export const selectEditor = (state: RootState) => {
+  return state.codeEditor.editorInstance;
+};
 
 export default CodeEditorSlice.reducer;
